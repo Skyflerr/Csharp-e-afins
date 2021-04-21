f39c613e4081935a4ab13e1852b956a16aed37e3
Eduardo Skye <skyemails1@gmail.com>
2021-04-20 21:48:27 -0300

    first commit

---
+++ Avaliação LP2/Função Linear.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avaliação_Questão_9
+{
+    class FuncaoLinear
+    {
+        private double CoeficienteAngular;
+        public double CoeficienteLinear;
+
+        public FuncaoLinear(double x ,double y)
+        {
+            SetCoeficienteAngular(x);
+            CoeficienteLinear = y;
+        }
+
+        public bool SetCoeficienteAngular(double x)
+        { // restringe o coeficiente angular de ser igual a 0 
+            if (x != 0)
+            {
+                CoeficienteAngular = x;
+                return true;
+            }
+
+            return false;
+        }
+
+        public double GetCoeficienteAngular()
+        {
+            return CoeficienteAngular;
+        }
+
+        public double root() //piadinhas com raiz e talz
+        {
+            double resultado = (CoeficienteLinear = CoeficienteLinear * (-1)) / CoeficienteAngular;
+            return resultado;
+        }
+
+        public string inclinação()
+        {
+            string resposta = "";
+
+            if (CoeficienteAngular > 0)
+            {
+                resposta = "Crescente";
+            }
+
+            else
+            {
+                resposta = "Decrecente";
+            }
+            return resposta;
+        }
+        public string descricao()
+        {
+            string descricao = "f(x) = "+ CoeficienteAngular + "x " + "+ " + CoeficienteLinear;
+            return descricao;
+        }
+    }
+}
---
+++ Avaliação LP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Avaliação_Questão_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Função linear ,digite o valor A : ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Agora digite o B : ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            FuncaoLinear funcao = new FuncaoLinear(a,b);
+
+            if (funcao.SetCoeficienteAngular(a) == false)
+            {
+                Console.WriteLine("Parece que seu 'a' é igual a zero :( ");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.Write("Aqui esta : " + funcao.descricao() +"\n");
+                Console.Write("A inclinação é : "+ funcao.inclinação() + "\n");
+                Console.Write("E a raiz é : "+ funcao.root() + "\n");
+                Console.ReadLine();
+            }
+        }
+    }
+}
